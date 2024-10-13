59fcba462ac8e922a577fc825c3a144b74334d45
Diogozap <diogorodrigueszap.0903@gmail.com>
2024-10-12 21:42:23 -0300

    Update ProductForm.js

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,37 +1,34 @@
-import React, { useState } from "react"; // Importando React e useState
-import axios from "axios"; // Importando axios para fazer requisições HTTP
+import React, { useState } from "react";
+import axios from "axios";
 
 const ProductForm = ({ fetchProdutos }) => {
-    const [nome, setNome] = useState(""); // Estado para armazenar o nome do produto
-    const [categoria, setCategoria] = useState(""); // Estado para armazenar a categoria do produto
-    const [preco, setPreco] = useState(""); // Estado para armazenar o preço do produto
-    const [quantidadeEmEstoque, setQuantidadeEmEstoque] = useState(""); // Estado para armazenar a quantidade em estoque
+    const [nome, setNome] = useState(""); 
+    const [categoria, setCategoria] = useState("");
+    const [preco, setPreco] = useState(""); 
+    const [quantidadeEmEstoque, setQuantidadeEmEstoque] = useState("");
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Previne o comportamento padrão do formulário
+        e.preventDefault(); 
         try {
-            // Envia os dados do novo produto para a API
             const response = await axios.post(
                 "http://127.0.0.1:8000/api/produtos/",
                 {
                     nome,
                     categoria,
                     preco,
-                    quantidade_em_estoque: quantidadeEmEstoque, // Ajuste o nome para o que está na API
+                    quantidade_em_estoque: quantidadeEmEstoque, 
                 }
             );
-            console.log("Produto adicionado:", response.data); // Loga o produto adicionado
+            console.log("Produto adicionado:", response.data); 
 
-            // Chama fetchProdutos para atualizar a lista de produtos
             fetchProdutos();
 
-            // Limpa os campos após a adição
             setNome("");
             setCategoria("");
             setPreco("");
             setQuantidadeEmEstoque("");
         } catch (error) {
-            console.error("Erro ao adicionar produto:", error); // Loga o erro no console
+            console.error("Erro ao adicionar produto:", error); 
         }
     };
 
@@ -81,4 +78,4 @@ const ProductForm = ({ fetchProdutos }) => {
     );
 };
 
-export default ProductForm; // Exporta o componente
+export default ProductForm; 
