81755d93267cb908429f7fc3ef6af63cffb2c620
Diogozap <diogorodrigueszap.0903@gmail.com>
2024-10-12 21:38:54 -0300

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -13,13 +13,14 @@ const ProductList = ({ produtos, fetchProdutos }) => {
         return <div>{error}</div>;
     }
 
+   
     return (
-        <div>
+        <div className="container">
             <h1>Lista de Produtos</h1>
             <Link to="/add">Adicionar Produto</Link>{" "}
             {produtos.length > 0 ? (
                 produtos.map((produto) => (
-                    <div key={produto.id}>
+                    <div className="product-item" key={produto.id}>
                         <h2>{produto.nome}</h2>
                         <p>Categoria: {produto.categoria}</p>
                         <p>Preço: R${produto.preco}</p>
